cbe6cee5fb1fe9966606d51d74566baa6d40dce1
ramanujds <ramanujds9@gmail.com>
2025-07-29 17:49:52 +0530

    added test.java

---
+++ Test.java
@@ -0,0 +1,9 @@
+class Test{
+    public static void main(String[] args) {
+        // Create an instance of the class
+        Test test = new Test();
+
+        // Call the method to print the message
+        test.printMessage();
+    }
+}
