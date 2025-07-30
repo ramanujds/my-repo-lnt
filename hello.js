65980bc0ad9d657f55e31b5716d7837e1013cab0
ramanujds <ramanujds9@gmail.com>
2025-07-30 10:01:44 +0530

    updated some work

--- hello.js
+++ hello.js
@@ -1,3 +1,17 @@
 function play() {
     console.log("Hello, World!");
 }
+
+function greet(name) {
+    console.log(`Hello, ${name}!`);
+}
+
+function farewell(name) {
+    console.log(`Goodbye, ${name}!`);
+}
+
+function main() {
+    play();
+    greet("Alice");
+    farewell("Bob");
+}
